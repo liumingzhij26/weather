4c813428dad20566799265b85a881e6d90a39ada
liumingzhi <liumingzhij26@qq.com>
2019-11-06 17:30:29 +0800

    feat: debug test

---
+++ src/Test.php
@@ -0,0 +1,17 @@
+<?php
+/***************************************************************************
+ *
+ * Copyright (c) 2019 liumingzhi, Inc. All Rights Reserved
+ *
+ **************************************************************************
+ *
+ * @file Test.php
+ * @author liumingzhi(liumingzhij26@gmail.com)
+ * @date 2019-11-06 17:29:00
+ *
+ **/
+
+namespace Overtrue\Weather;
+class Test{
+
+}
